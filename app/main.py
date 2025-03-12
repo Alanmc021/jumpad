eaf7a1c3c8eb1dc7dee20538381fc8378d082f4f
Seu Nome <seu@email.com>
2025-03-12 17:04:29 -0300

    feat(app): create mvc

--- app/main.py
+++ app/main.py
@@ -1,3 +1,29 @@
+# from fastapi import FastAPI
+# from app.routes.math_routes import router
+
+# app = FastAPI(
+#     title="Math API",
+#     description="API RESTful para operações matemáticas (soma e média)",
+#     version="1.0.0",
+#     docs_url="/docs",
+#     redoc_url="/redoc"
+# )
+
+# # Inclui as rotas
+# app.include_router(router, prefix="/math", tags=["Math Operations"])
+
+# # Health Check
+# @app.get("/", tags=["Health Check"])
+# def root():
+#     return {"message": "Math API is running!"}
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+ 
+
+import os
+import uvicorn
 from fastapi import FastAPI
 from app.routes.math_routes import router
 
@@ -18,7 +44,6 @@ def root():
     return {"message": "Math API is running!"}
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
- 
+    port = int(os.getenv("PORT", 8000))  # Render define automaticamente a porta na variável de ambiente PORT
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
  
--- app/models/numbers.py
+++ app/models/numbers.py
@@ -1,36 +1,3 @@
-# from pydantic import BaseModel, Field, ConfigDict
-# from typing import List
-
-# class MathRequest(BaseModel):
-#     numbers: List[float] = Field(..., description="List of numbers to be processed")
-
-#     model_config = ConfigDict(json_schema_extra={
-#         "example": {
-#             "numbers": [1.2, 3.4, 5.6]
-#         }
-#     })
-
-# from pydantic import BaseModel, Field, field_validator
-# from typing import List
-
-# class MathRequest(BaseModel):
-#     numbers: List[float] = Field(..., description="Lista de números a serem processados")
-
-#     @field_validator("numbers")
-#     @classmethod
-#     def validate_numbers(cls, v):
-#         if not v:
-#             raise ValueError("A lista de números não pode estar vazia")
-#         if any(not isinstance(n, (int, float)) for n in v):
-#             raise ValueError("Todos os valores devem ser números (int ou float)")
-#         return v
-
-#     class Config:
-#         json_schema_extra = {
-#             "example": {"numbers": [1.2, 3.4, 5.6]}
-#         }
-
-
 from pydantic import BaseModel, Field, field_validator, ConfigDict
 from typing import List
 
